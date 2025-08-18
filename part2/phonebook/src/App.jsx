5e8e9180abf9580283bdffc8e745961e2d1e18d8
not6248 <96365700+not6248@users.noreply.github.com>
2025-08-18 22:09:18 +0700

    3.13-3.18

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,81 +1,91 @@
-import { useEffect, useState } from "react";
-import Filter from "./components/Filter";
-import PersonForm from "./components/PersonForm";
-import Persons from "./components/Persons";
-import personService from "./services/persons";
-import Notification from "./components/Notification";
+import { useEffect, useState } from "react"
+import Filter from "./components/Filter"
+import PersonForm from "./components/PersonForm"
+import Persons from "./components/Persons"
+import personService from "./services/persons"
+import Notification from "./components/Notification"
 
 const App = () => {
-  const [persons, setPersons] = useState([]);
-  const [newName, setNewName] = useState("");
-  const [newNumber, setNewNumber] = useState("");
-  const [filter, setFilter] = useState("");
+  const [persons, setPersons] = useState([])
+  const [newName, setNewName] = useState("")
+  const [newNumber, setNewNumber] = useState("")
+  const [filter, setFilter] = useState("")
   const [message, setMessage] = useState(null)
   const [errormessage, setErrorMessage] = useState(null)
 
   useEffect(() => {
-    personService.getAll().then(initialPersons => {
+    personService.getAll().then((initialPersons) => {
       setPersons(initialPersons)
     })
-  }, []);
+  }, [])
 
   const handleNameChange = (event) => {
-    setNewName(event.target.value);
-  };
+    setNewName(event.target.value)
+  }
 
   const handleNumberChange = (event) => {
-    setNewNumber(event.target.value);
-  };
+    setNewNumber(event.target.value)
+  }
 
   const handleFilterChange = (event) => {
-    setFilter(event.target.value);
-  };
+    setFilter(event.target.value)
+  }
 
   const addPerson = (event) => {
-    event.preventDefault();
+    event.preventDefault()
 
     const findPerson = persons.find((person) => person.name === newName)
     if (findPerson) {
-      if(confirm(`${findPerson.name} is already added to phonebiik, repiace the old number with a new one`)){
-        const changedPerson = {...findPerson , number:newNumber}
+      if (
+        confirm(
+          `${findPerson.name} is already added to phonebook, replace the old number with a new one`
+        )
+      ) {
+        const changedPerson = { ...findPerson, number: newNumber }
         personService
-          .update(findPerson.id,changedPerson)
-          .then(returnedPerson => {
-            setPersons(persons.map(person => person.id !== findPerson.id ? person : returnedPerson))
-            setNewName("");
-            setNewNumber("");
+          .update(findPerson.id, changedPerson)
+          .then((returnedPerson) => {
+            setPersons(
+              persons.map((person) =>
+                person.id !== findPerson.id ? person : returnedPerson
+              )
+            )
+            setNewName("")
+            setNewNumber("")
             setMessage(`Updated ${returnedPerson.name}`)
             setTimeout(() => {
               setMessage(null)
-            }, 5000);
+            }, 5000)
           })
           .catch(() => {
-            setErrorMessage(`infomation of ${findPerson.name} has already been removed from server`)
+            setErrorMessage(
+              `infomation of ${findPerson.name} has already been removed from server`
+            )
             setTimeout(() => {
               setErrorMessage(null)
-            }, 5000);
+            }, 5000)
           })
       }
-      return;
+      return
     }
 
     const person = {
       name: newName,
       number: newNumber,
-    };
-    personService.create(person).then(returnedPerson => {
-      setPersons(persons.concat(returnedPerson));
-      setNewName("");
-      setNewNumber("");
+    }
+    personService.create(person).then((returnedPerson) => {
+      setPersons(persons.concat(returnedPerson))
+      setNewName("")
+      setNewNumber("")
       setMessage(`Added ${returnedPerson.name}`)
       setTimeout(() => {
         setMessage(null)
-      }, 5000);
-    });
-  };
+      }, 5000)
+    })
+  }
 
-  const deletePerson = (id,name) => {
-    if(confirm(`Delete ${name} ?`)){
+  const deletePerson = (id, name) => {
+    if (confirm(`Delete ${name} ?`)) {
       personService.deleteData(id).then(() => {
         setPersons(persons.filter((p) => p.id !== id))
       })
@@ -87,13 +97,13 @@ const App = () => {
       ? persons
       : persons.filter((person) =>
           person.name.toLowerCase().includes(filter.toLowerCase())
-        );
+        )
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <Notification message={message}/>
-      <Notification message={errormessage} isError={true}/>
+      <Notification message={message} />
+      <Notification message={errormessage} isError={true} />
       <Filter filter={filter} handleFilterChange={handleFilterChange} />
       <h3>add a new</h3>
       <PersonForm
@@ -106,7 +116,7 @@ const App = () => {
       <h3>Numbers</h3>
       <Persons persons={filterPersons} deletePerson={deletePerson} />
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
--- part3/PhonebookBackend/index.js
+++ part3/PhonebookBackend/index.js
@@ -1,77 +1,72 @@
+require("dotenv").config()
 const express = require("express")
-var morgan = require('morgan')
+const Person = require("./models/person")
+var morgan = require("morgan")
 const app = express()
 
-morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
-
-app.use(express.static('dist'))
-app.use(express.json())
-app.use(morgan(function (tokens, req, res) {
-  return [
-    tokens.method(req, res),
-    tokens.url(req, res),
-    tokens.status(req, res),
-    tokens.res(req, res, 'content-length'), '-',
-    tokens['response-time'](req, res), 'ms',
-    tokens.body(req, res)
-  ].join(' ')
-}))
-
-let persons = [
-  {
-    id: "1",
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: "2",
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: "3",
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: "4",
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-]
-
-app.get("/api/persons", (request, response) => {
-  response.json(persons)
+morgan.token("body", function (req, res) {
+  return JSON.stringify(req.body)
 })
 
-app.get("/info", (request, response) => {
+app.use(express.static("dist"))
+app.use(express.json())
+app.use(
+  morgan(function (tokens, req, res) {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, "content-length"),
+      "-",
+      tokens["response-time"](req, res),
+      "ms",
+      tokens.body(req, res),
+    ].join(" ")
+  })
+)
+
+app.get("/api/persons", (request, response, next) => {
+  Person.find({})
+    .then((persons) => {
+      response.json(persons)
+    })
+    .catch((error) => next(error))
+})
+
+app.get("/info", (request, response, error) => {
   const date = new Date()
   let dateText = date.toString()
 
-  response.send(
-    `<p>Phonebook has info for ${persons.length} people</p><p>${dateText}</p>`
-  )
+  Person.find({})
+    .then((persons) => {
+      response.send(
+        `<p>Phonebook has info for ${persons.length} people</p><p>${dateText}</p>`
+      )
+    })
+    .catch((error) => next(error))
 })
 
-app.get("/api/persons/:id", (request, response) => {
-  const id = request.params.id
-  const person = persons.find((person) => person.id === id)
-
-  if (person) {
-    response.json(person)
-  } else {
-    response.status(404).end()
-  }
+app.get("/api/persons/:id", (request, response, next) => {
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch((error) => next(error))
 })
 
-app.delete("/api/persons/:id", (request, response) => {
-  const id = request.params.id
-  persons = persons.filter((person) => person.id !== id)
-
-  response.status(204).end()
+app.delete("/api/persons/:id", (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then((result) => {
+      response.status(204).end()
+    })
+    .catch((error) => next(error))
 })
 
-app.post("/api/persons", (request, response) => {
+app.post("/api/persons", (request, response, error) => {
   const randomId = Math.floor(Math.random() * 9999999999)
   const body = request.body
 
@@ -81,25 +76,52 @@ app.post("/api/persons", (request, response) => {
     })
   }
 
-  const matchName = persons.find((person) => person.name === body.name)
-
-  if (matchName) {
-    return response.status(400).json({
-      error: "name must be unique",
-    })
-  }
-
-  const person = {
-    id: String(randomId),
+  const person = Person({
     name: body.name,
     number: body.number,
-  }
-  
-  persons = persons.concat(person)
-  response.json(person)
+  })
+
+  person
+    .save()
+    .then((savedPerson) => {
+      response.json(savedPerson)
+    })
+    .catch((error) => next(error))
 })
 
-const PORT = process.env.PORT || 3001
+app.put("/api/persons/:id", (request, response, next) => {
+  const { name, number } = request.body
+
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (!person) {
+        return response.status(404).end()
+      }
+
+      person.name = name
+      person.number = number
+
+      return person.save().then((updatedPerson) => {
+        response.json(updatedPerson)
+      })
+    })
+    .catch((error) => next(error))
+})
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" })
+  }
+
+  next(error)
+}
+
+// handler of requests with result to errors
+app.use(errorHandler)
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
