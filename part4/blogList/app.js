44d441dd509dc83fe2514a19911e6779ff295c75
not6248 <96365700+not6248@users.noreply.github.com>
2025-09-22 22:41:39 +0700

    4.15 Implement a way to create new users

--- part4/blogList/app.js
+++ part4/blogList/app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 const middleware = require('./utils/middleware')
 
 const app = express()
@@ -17,7 +18,10 @@ mongoose
   })
 
 app.use(express.json())
+
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
+
 app.use(middleware.errorHandler)
 
 module.exports = app
