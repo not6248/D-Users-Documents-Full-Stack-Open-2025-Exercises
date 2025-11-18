afc188000fa5c6934c45cad63ce00dbf5e79e02d
not6248 <96365700+not6248@users.noreply.github.com>
2025-11-18 21:20:40 +0700

    5.19 Create a test that verifies that a logged in user can create a blog

--- part5/playwright/tests/example.spec.js
+++ part5/playwright/tests/example.spec.js
@@ -15,32 +15,54 @@ describe('Blog app', () => {
   })
 
   test('Login form is shown', async ({ page }) => {
-    await page.getByLabel('username').fill("mluukkai")
-    await page.getByLabel('password').fill("salainen")
+    await page.getByLabel('username').fill('mluukkai')
+    await page.getByLabel('password').fill('salainen')
     await page.getByRole('button', { name: 'login' }).click()
     await expect(page.getByText('Matti Luukkainen logged in')).toBeVisible()
   })
 
-    describe('Login', () => {
-      test('succeeds with correct credentials', async ({ page }) => {
-        await page.getByLabel('username').fill("mluukkai")
-        await page.getByLabel('password').fill("salainen")
-        await page.getByRole('button', { name: 'login' }).click()
-        await expect(page.getByText('Matti Luukkainen logged in')).toBeVisible()
-      })
+  describe('Login', () => {
+    test('succeeds with correct credentials', async ({ page }) => {
+      await page.getByLabel('username').fill('mluukkai')
+      await page.getByLabel('password').fill('salainen')
+      await page.getByRole('button', { name: 'login' }).click()
+      await expect(page.getByText('Matti Luukkainen logged in')).toBeVisible()
+    })
 
-      test('fails with wrong credentials', async ({ page }) => {
-        await page.getByLabel('username').fill("userwrong")
-        await page.getByLabel('password').fill("wrong")
-        await page.getByRole('button', { name: 'login' }).click()
+    test('fails with wrong credentials', async ({ page }) => {
+      await page.getByLabel('username').fill('userwrong')
+      await page.getByLabel('password').fill('wrong')
+      await page.getByRole('button', { name: 'login' }).click()
 
-        const errorDiv = page.locator('.error')
-        await expect(errorDiv).toContainText('wrong credentials')
-        await expect(errorDiv).toHaveCSS('border-style', 'solid')
-        await expect(errorDiv).toHaveCSS('color', 'rgb(255, 0, 0)')
+      const errorDiv = page.locator('.error')
+      await expect(errorDiv).toContainText('wrong credentials')
+      await expect(errorDiv).toHaveCSS('border-style', 'solid')
+      await expect(errorDiv).toHaveCSS('color', 'rgb(255, 0, 0)')
 
-        await expect(page.getByText('Matti Luukkainen logged in')).not.toBeVisible()
-      })
+      await expect(
+        page.getByText('Matti Luukkainen logged in')
+      ).not.toBeVisible()
+    })
   })
 
+  describe('When logged in', () => {
+    beforeEach(async ({ page }) => {
+      await page.getByLabel('username').fill('mluukkai')
+      await page.getByLabel('password').fill('salainen')
+      await page.getByRole('button', { name: 'login' }).click()
+    })
+
+    test('a new blog can be created', async ({ page }) => {
+        await page.getByRole('button', { name: 'create new blog' }).click()
+        await page.getByRole('textbox', { name: 'title:' }).fill("a blog created by playwright")
+        await page.getByRole('textbox', { name: 'author:' }).fill("mluukkai")
+        await page.getByRole('textbox', { name: 'url:' }).fill("www.playwright.com")
+        await page.getByRole('button', { name: 'create' }).click()
+        await page.getByText("a blog created by playwright mluukkai viewhide").waitFor()
+        await expect(page.getByText('a blog created by playwright mluukkai viewhide')).toBeVisible()
+
+        const addNotiDiv = page.locator('.add')
+        await expect(addNotiDiv).toContainText('a new blog a blog created by playwright')
+    })
+  })
 })
