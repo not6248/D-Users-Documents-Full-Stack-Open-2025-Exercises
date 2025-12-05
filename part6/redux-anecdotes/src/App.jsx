2cc45d23de56acecde063d3841dec1c80a2267c6
not6248 <96365700+not6248@users.noreply.github.com>
2025-12-05 22:42:58 +0700

    6.5 ordered by the number of votes

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -3,7 +3,7 @@ import { createAnecdotes, voteOf } from './reducers/anecdoteReducer'
 
 const App = () => {
   const dispatch = useDispatch()
-  const anecdotes = useSelector((state) => state)
+  const anecdotes = useSelector((state) => state).sort((a , b) => b.votes - a.votes)
 
   const vote = (id) => {
     console.log('vote', id)
