01ae73ddc880314dfbee9ad6eb5f62879d2a69d4
not6248 <96365700+not6248@users.noreply.github.com>
2025-12-17 21:30:31 +0700

    6.14,6.15 Use json-server and Use the Fetch API getaAll and createNew

--- part6/redux-anecdotes/src/components/AnecdoteForm.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,13 +1,16 @@
 import { useDispatch } from 'react-redux'
 import { createAnecdotes } from '../reducers/anecdoteReducer'
+import anecdoteService from '../services/anecdotes'
 
 const AnecdoteForm = () => {
   const dispatch = useDispatch()
-  const addAnecdote = (event) => {
+
+  const addAnecdote = async (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
-    dispatch(createAnecdotes(content))
+    const newAnecdote = await anecdoteService.createNew(content)
+    dispatch(createAnecdotes(newAnecdote))
   }
 
   return (
