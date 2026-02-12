033b87d3a93d0534ee86915be2a4d1a7bf1ed2ad
not6248 <96365700+not6248@users.noreply.github.com>
2026-02-12 21:45:25 +0700

    7.8 Ultimate Hooks

--- part7/ultimate-hooks/src/App.jsx
+++ part7/ultimate-hooks/src/App.jsx
@@ -18,10 +18,19 @@ const useField = (type) => {
 const useResource = (baseUrl) => {
   const [resources, setResources] = useState([])
 
-  // ...
+  useEffect(()=>{
+    const getAll = async () => {
+      const response = await axios.get(baseUrl)
+      setResources(response.data)
+    }
 
-  const create = (resource) => {
-    // ...
+    getAll();
+  },[baseUrl])
+
+  const create = async (resource) => {
+    const response = await axios.post(baseUrl, resource)
+    setResources((data) => [...data,response.data])
+    return response.data
   }
 
   const service = {
